af11dc54269c488c4ded39164bc3baf438d8cb0a
Lazarus1993 <ashutoshburde@gmail.com>
2019-02-04 14:18:04 -0500

    Guess_the_Number

---
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -0,0 +1,43 @@
+package academy.learnprogramming.config;
+
+import academy.learnprogramming.GuessCount;
+import academy.learnprogramming.MaxNumber;
+import academy.learnprogramming.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+@PropertySource("classpath:config/game.properties")
+public class GameConfig {
+    // == fields ==
+    @Value("${game.maxNumber:20}")
+    private int maxNumber = 25;
+
+    @Value("${game.minNumber:20}")
+    private int minNumber = 0;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount = 8;
+    // == bean methods ==
+    @Bean
+    @MaxNumber
+    public int maxNumber(){
+        return maxNumber;
+    }
+
+    @Bean
+    @MinNumber
+    public int minNumber(){
+        return minNumber;
+    }
+
+    @Bean
+    @GuessCount
+    public int guessCount(){
+        return guessCount;
+    }
+}
